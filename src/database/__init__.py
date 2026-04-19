b8532dfc302ad59d52233c6861e822f092fbbbf2
Rômulo Soares <romulolassoares@gmail.com>
2026-04-19 11:41:55 -0300

    refactor(database): expose DatabaseFactory and Database in __init__

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,7 +1,12 @@
 from .duckdb import DuckDB
 from .sql_server import SQLServerConnection
+from .database_factory import DatabaseFactory
+from .database import Database
+
 
 all = [
     "SQLServerConnection",
     "DuckDB",
+    "DatabaseFactory",
+    "Database",
 ]
